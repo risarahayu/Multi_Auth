028e7e1d95597643d2a4419ac416f725ac786796
risarahayu <rrisa329@gmail.com>
2023-03-04 20:05:08 +0800

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+
+class HomeController extends Controller
+{
+    public function index(){
+        $role=Auth::user()->role;
+        if ($role=='1'){
+            return view('admin.dashboard');
+        }
+        else {
+            return view('dashboard');
+        }
+    }
+}
